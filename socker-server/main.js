99dbcb795c14d2c4a99385b2ed38408d235e1974
Nguyễn Ngọc Minh <minhngoc61021@gmail.com>
2022-04-06 15:20:04 +0700

    update

--- socker-server/main.js
+++ socker-server/main.js
@@ -6,9 +6,20 @@ const io = require('socket.io')(3001, {
 
 const userInfoList = [];
 io.on("connection", (socket) => {
-  // send a message to the client
-  socket.on('USERNAME_SIGN_UP', userInfo => {
-      userInfoList.push(userInfo);
-      socket.emit('USER_LIST', userInfoList);
-  })
+    // send a message to the client
+    socket.on('USERNAME_SIGN_UP', userInfo => {
+        const isExist = userInfoList.some(user => user.username === userInfo.username)
+        if (isExist) {
+            return socket.emit('USER_ALREADY_EXIST');
+        }
+        socket.peerId = userInfo.peerId;
+        userInfoList.push(userInfo);
+        socket.emit('USER_LIST', userInfoList);
+        socket.broadcast.emit('NEW_USER', userInfo)
+    });
+    socket.on('disconnect', () => {
+        const index = userInfoList.findIndex((user => user.peerId === socket.peerId));
+        userInfoList.splice(index, 1);
+        io.emit('DISCONNECT_USER', socket.peerId);
+    })
 });
--- vite-project/main.js
+++ vite-project/main.js
@@ -3,7 +3,32 @@ import Peer from "peerjs";
 import $ from 'jquery';
 import io from 'socket.io-client';
 
-// const socker = io('http://localhost:3001');
+const socket = io('http://localhost:3001');
+
+
+$('#video-chat').hide();
+
+socket.on('USER_LIST', userList => {
+    $('#sign-up').hide();
+    $('#video-chat').show();
+    userList.forEach(userInfo => {
+        const {username, peerId} = userInfo;
+        $('#userList').append(`<li style="cursor: pointer" id="${peerId}">${username}</li>`);
+    })
+    socket.on('NEW_USER', newUser => {
+        const {username, peerId} = newUser;
+            $('#userList').append(`<li id="${peerId}">${username}</li>`);
+    });
+    socket.on('DISCONNECT_USER', peerId => {
+        $(`#${peerId}`).remove()
+    });
+
+})
+socket.on('USER_ALREADY_EXIST', () =>
+    alert("Nguoi dung da ton tai!!")
+);
+
+
 
 function openStream() {
     const config = {
@@ -34,10 +59,10 @@ const peer = new Peer();
 
 peer.on('open', id => {
     $('#peer-id').append(id)
-    // $('#btnSignup').click(() => {
-    //     let username = $('#txtUsername').val();
-    //     socker.emit('USERNAME_SIGN_UP', { username: username, peerId: id });
-    // })
+    $('#btnSignup').click(() => {
+        let username = $('#txtUsername').val();
+        socket.emit('USERNAME_SIGN_UP', { username: username, peerId: id });
+    })
 })
 
 // Caller
@@ -62,7 +87,23 @@ peer.on('call', call => {
     });
 })
 
-// socket.on('USER_LIST', userList => {
-//     console.log(userList);
-//
-// })
+$('#userList').on('click', 'li', function () {
+    console.log($(this).attr('id'));
+    const id = $('#peer-id').text();
+    const username = $("#" + $(this).attr('id')).text();
+    if ($(this).attr('id') !== id) {
+        if (confirm(`Do you want to video call with this user ${username}?`)) {
+            openStream()
+                .then(stream => {
+                    playStream('localStream', stream);
+                    const call = peer.call(id, stream);
+                    call.on('stream', remoteStream => {
+                        playStream('remoteStream', remoteStream)
+                    });
+                });
+        }
+    }
+    else {
+        alert(`You can't call yourself.`);
+    }
+})
